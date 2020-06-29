114683b8cdd5ece662a355f3d37213c9cead64d2
casa-a-takeshita <asuka.takeshita@casa-inc.co.jp>
2020-06-29 12:31:51 +0900

    Finish toy app
    Finish toy app

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_26_043254) do
+ActiveRecord::Schema.define(version: 2020_06_29_015550) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
